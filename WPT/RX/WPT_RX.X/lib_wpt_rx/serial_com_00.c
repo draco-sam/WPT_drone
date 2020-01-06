ad41e8c344cce6609c9e9c4e7e68f4229a6f89de
Samuel <you@example.com>
2020-01-06 22:42:15 +0100

    serial_com_00.c : I2C repeated start en cours...

--- WPT/RX/WPT_RX.X/lib_wpt_rx/serial_com_00.c
+++ WPT/RX/WPT_RX.X/lib_wpt_rx/serial_com_00.c
@@ -8,16 +8,20 @@
 
 #include "serial_com_00.h"
 
-static unsigned short   i2c_address                 = 0b1101000;//104.
+static unsigned short   i2c_slave_address                 = 0b1101000;//104.
 static unsigned short   flag_i2c_start              = 0;//Flag start pour code C.
+static unsigned short   flag_i2c_repeated_start     = 0;
 static unsigned short   flag_i2c_write_address      = 0;
 static unsigned short   flag_i2c_write_data         = 0;
+static unsigned short   flag_i2c_reception          = 0;
+static unsigned short   flag_i2c_rbf_complete       = 0;
 static unsigned short   i2c_data                    = 0;
 
 //Déclarations structures :
 static I2C2STATBITS     i2c_2_stat_bits;
 static I2C2TRNBITS      i2c_2_trn_bits;//I2C transmit register.
-static I2C2CONLBITS     i2c_2_conl_bits;//I2Cx CONTROL REGISTER LOW.
+static I2C2RCVBITS      i2c_2_rcv_bits;//I2C receive register.
+static I2C2CONLBITS     i2c_2_con_l_bits;//I2Cx CONTROL REGISTER LOW.
 
 void i2c_master_init(void)
 /*
@@ -43,7 +47,7 @@ void __attribute__ ( ( interrupt, no_auto_psv ) ) _MI2C2Interrupt ( void )
     IFS2bits.MI2C2IF = 0;//Reset flag master I2C 2.
     
     //Interruption après start condition.
-    if(flag_i2c_start == 1)
+    if(flag_i2c_start == 1 && flag_i2c_repeated_start == 0)
     {
         //S bit at "1" et P bit at "0".
         //Master module drives SCL2 low.
@@ -51,38 +55,60 @@ void __attribute__ ( ( interrupt, no_auto_psv ) ) _MI2C2Interrupt ( void )
         //2)Ecrire adresse dans I2C2TRN registre :
         if(flag_i2c_write_address == 0 && flag_i2c_write_data == 0)
         {
-            flag_i2c_write_address = 1;
-            
-            //Vérifier si bus libre.
-            if(i2c_2_stat_bits.TRSTAT == 0)//0 : Master transmit is not in progress.
-            {
-                i2c_2_trn_bits.I2CTXDATA = i2c_address;
-            }
-            //si non attendre ????
+            flag_i2c_write_address      = 1;
+            i2c_2_trn_bits.I2CTXDATA    = i2c_slave_address;
         }
         //4)Vérifier ACK sur clock n°9 :
         else if(flag_i2c_write_address == 1 && flag_i2c_write_data == 0)
         {
             if(i2c_2_stat_bits.ACKSTAT == 0)//0 = Acknowledge was received from Slave.
             {
-                //SCLx remains low until the next event.
+                //Envoyer la commande :
+                flag_i2c_write_data         = 1;
+                i2c_2_trn_bits.I2CTXDATA    = i2c_data;
             }
             else
             {
-                //PAS recu ACK slave, arreter et reseter tout :
-                i2c_2_conl_bits.PEN     = 1;//1 = Initiates Stop condition on SDAx and SCLx pins.
-                flag_i2c_start          = 0;
-                flag_i2c_write_address  = 0;
-                flag_i2c_write_data     = 0;
-                i2c_data                = 0;
-                
-            }
-            
+                //PAS recu ACK slave, arreter et reseter tout.
+                i2c_stop_com();                                
+            } 
         }
-        
-        
-        
-        
+        else if(flag_i2c_write_address == 1 && flag_i2c_write_data == 1)
+        {
+            if(i2c_2_stat_bits.ACKSTAT == 0)//0 = Acknowledge was received from Slave.
+            {
+                //Envoyer repeated start :
+                flag_i2c_repeated_start = 1;
+                flag_i2c_write_address  = 0;//Reset flag pour repeated start.
+                i2c_2_con_l_bits.RSEN   = 1;
+            }
+            else
+            {
+                //PAS recu ACK slave, arreter et reseter tout.
+                i2c_stop_com();                                
+            }
+        }  
+    }
+    else if(flag_i2c_start == 0 && flag_i2c_repeated_start == 1)
+    {
+        if(flag_i2c_write_address == 0)
+        {
+            flag_i2c_write_address      = 1;
+            i2c_2_trn_bits.I2CTXDATA    = i2c_slave_address;
+        }
+        else//flag_i2c_write_address = 1.
+        {
+            if(flag_i2c_rbf_complete == 0 && i2c_2_stat_bits.RBF == 0)
+            {
+                i2c_2_con_l_bits.RCEN = 1;
+            }
+            else if()
+            {
+                //
+            }
+        }
+            
+            
         
     }
 }
@@ -93,19 +119,39 @@ void i2c_master_write(unsigned short data)
  * 
  */
 {  
-    //Vérifier si ancien stop détecté :
-    if(i2c_2_stat_bits.P == 1)//1 = Indicates that a Stop bit has been detected last.
+    /*
+     * Vérifier si :
+     *      - Bus libre.
+     *      - Stop bit anciennement détecté.
+     */
+    if(i2c_2_stat_bits.TRSTAT == 0 && i2c_2_stat_bits.P == 1)
     {
         flag_i2c_start = 1;
         //flag_i2c_write_address = 1;
         
         //1)Générer condition de start :
-        i2c_2_conl_bits.SEN = 1;//Initiates Start condition on SDAx and SCLx pins.
+        i2c_2_con_l_bits.SEN = 1;//Initiates Start condition on SDAx and SCLx pins.
         
         //Sauvegarder data pour routine de service d'intérruption.
         i2c_data = data;
     }
+    //si non attendre ????
     
     
+    
+}
+//**************************************************************************************************
+
+//static i2c_stop_com(void)
+void i2c_stop_com(void)
+/*
+ * 
+ */
+{
+    i2c_2_con_l_bits.PEN    = 1;//1 = Initiates Stop condition on SDAx and SCLx pins.
+    flag_i2c_start          = 0;
+    flag_i2c_write_address  = 0;
+    flag_i2c_write_data     = 0;
+    flag_i2c_reception      = 0;
 }
 //**************************************************************************************************
