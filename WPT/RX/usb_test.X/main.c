87a0f3fd32ad40ae268f5d868064e364ebb98205
Samuel <you@example.com>
2020-02-05 22:18:25 +0100

    USB COM3, main.c : Code plus propre avec juste "CDCTxService()" dans la while principale.
    - Gère les \r et \n
    - Titre souligné.

--- WPT/RX/usb_test.X/main.c
+++ WPT/RX/usb_test.X/main.c
@@ -53,10 +53,9 @@
                          Main application
  */
 
+void integer_to_ascii(unsigned short data,char *table);
 
-
-int main(void)
-{
+int main(void){
     #define led_red         LATGbits.LATG7
     #define led_green       LATGbits.LATG6
     #define led_blue        LATEbits.LATE7
@@ -64,8 +63,8 @@ int main(void)
     #define off             1
     
     unsigned long       counter_while           = 0;
-    char                data_com                = "USB Virtual COM3";
     
+
     // initialize the device
     SYSTEM_Initialize();
     
@@ -89,45 +88,29 @@ int main(void)
     led_green   = off;
     led_blue    = off;
     
-    while(counter_while < 1e+6){
+    while(counter_while < 500e+3){
         counter_while++; 
     }
     led_red     = on;
     led_green   = off;
    
-      
-    unsigned short flag_tx      = 0;
-    unsigned short flag_tx_2    = 0;
-    unsigned short flag_tx_3    = 0;
+    //0x0D : Carriage return.
+    //0x0A : Line feed.
+    char data_4[]   = {0x0d,0x0a};
+    char table[5]   = {'0'};//Tableau pour tester la conversion vers ascii.
     
-    int data_2[1] = {0x0d};//0x0D : Carriage return.
-    int data_3[1] = {0x0a};//0x0A : Line feed.
+    integer_to_ascii(54321,table);
+    Nop();
+    Nop();
+    
+    char            data_com[]      = "USB Virtual COM3 :\n\r-----------------\n\r";
+    unsigned int    flag_tx         = 0;
+    
+    while(USBUSARTIsTxTrfReady() != true){}
+    putsUSBUSART(data_com);
     
     while (1){
         //MCC_USB_CDC_DemoTasks();
-        
-        if(flag_tx == 0){
-            if(USBUSARTIsTxTrfReady() == true){
-                char data[] = "Hello World :";
-                putsUSBUSART(data);
-                flag_tx     = 1;
-                flag_tx_2   = 1;
-            }
-        }
-        if(flag_tx_2 == 1){
-            if(USBUSARTIsTxTrfReady() == true){
-                putsUSBUSART(data_2);
-                flag_tx_2 = 0;
-                flag_tx_3 = 1;
-            }
-        }
-        if(flag_tx_3 == 1){
-            if(USBUSARTIsTxTrfReady() == true){
-                putsUSBUSART(data_3);
-                flag_tx_3 = 0;
-            }
-        }
-        
         CDCTxService();
         
 
@@ -137,7 +120,35 @@ int main(void)
 
     return 1;
 }
-/**
- End of File
-*/
+//__________________________________________________________________________________________________
 
+void integer_to_ascii(unsigned short data,char *table){
+/*
+ * data max : 65535.
+ */
+    //unsigned short  table_lenght        = 0;
+    unsigned short  data_1              = 0;
+    unsigned short  data_2              = 0;
+    char            table_ascii[11]     = {'0','1','2','3','4','5','6','7','8','9'};
+    
+    //table_lenght = sizeof(table);
+    
+    //Ex : 54321.
+    data_1      = data / 10;    //54321 / 10 = 5432.
+    data_2      = data_1 * 10;  //5432 * 10 = 54320.
+    table[4]    = table_ascii[data - data_2]; //54321 - 54320 = 1.
+
+    data_2      = (data_1 / 10) * 10;//(5432 / 10) * 10 = 5430.
+    table[3]    = table_ascii[data_1 - data_2]; //5432 - 5430 = 2.
+    
+    data_1      = data_1 / 10;//5432 / 10 = 543.
+    data_2      = (data_1 / 10) * 10;//(543 / 10) * 10 = 540.
+    table[2]    = table_ascii[data_1 - data_2]; //543 - 540 = 3.
+    
+    data_1      = data_1 / 10;//543 / 10 = 54.
+    data_2      = (data_1 / 10) * 10;//(54 / 10) * 10 = 50.
+    table[1]    = table_ascii[data_1 - data_2]; //54 - 50 = 4.
+    
+    table[0]    = table_ascii[data_1 / 10]; //54 / 10 = 5.
+}
+//__________________________________________________________________________________________________
